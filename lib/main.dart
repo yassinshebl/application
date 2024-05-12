64dcb93826afde1c012e842fafe6836593c22b14
Yassin Shebl <yassinshebl2003@gmail.com>
2024-05-13 00:01:09 +0300

    updated the app to open the login page

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'University App',
-      home: Test(),
+      home: Login(),
       debugShowCheckedModeBanner: false,
     );
   }
